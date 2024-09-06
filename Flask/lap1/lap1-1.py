49f1c94e3c7c8bbc4b7b300e78ea63897e832d8c
EslamRedaMohamed <eslamreda.info@gmail.com>
2024-09-06 19:30:49 +0300

    flask lap1 task2

---
+++ Flask/lap1/lap1-1.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+students = [
+    {
+        "name": "John Doe",
+        "id": 1,
+        "major": "Computer Science"
+    },
+    {
+        "name": "Jane Smith",
+        "id": 2,
+        "major": "Mathematics"
+    },
+    {
+        "name": "Emily Johnson",
+        "id": 3,
+        "major": "Physics"
+    }
+]
+
+@app.route('/students')
+def students_page():
+    return render_template('index.html', students=students) 
+
+@app.route('/search/<int:id>')
+def search(id):
+    return render_template('search.html', students=students, id=id)
+
+
+if __name__ == "__main__":
+    print(app.url_map)
+    app.run(debug=True, port=5000)
